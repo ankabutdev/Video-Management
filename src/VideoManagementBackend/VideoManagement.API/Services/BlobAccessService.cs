e57437aea6589073e5e841013e73d439db9f0cbe
ankabutdev <Ankabut571@gmail.com>
2024-04-06 17:15:51 +0500

    complete

--- src/VideoManagementBackend/VideoManagement.API/Services/BlobAccessService.cs
+++ src/VideoManagementBackend/VideoManagement.API/Services/BlobAccessService.cs
@@ -4,6 +4,7 @@ namespace VideoManagement.API.Services;
 
 public class BlobAccessService
 {
+    // add you storage account and key
     protected readonly string _storageAccount = "cloudshell927410588";
     protected readonly string _key = "";
 
